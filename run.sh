f219da526e2c66e3579f343335493dc5fbf12cf3
Aaroh Gondane <aaroh.g@embitel.com>
2026-03-08 04:21:42 +0000

    Fix Azure dataset paths for DreamDrive input frames and checkpoints

---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DATA_PATH=$1
+
+apt-get update
+apt-get install -y libgl1
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+pip uninstall -y numpy
+pip install numpy==1.26.4
+
+cd submodules/diff-gaussian-rasterization
+pip install -e . --no-build-isolation
+cd ../../
+
+cd submodules/simple-knn
+pip install -e . --no-build-isolation
+cd ../../
+
+cd submodules/vista
+pip install -e . --no-build-isolation
+cd ../../
+
+# Copy checkpoints from Azure dataset
+mkdir -p submodules/vista/ckpts
+cp $DATA_PATH/checkpoints/* submodules/vista/ckpts/
+
+# Copy input frames from Azure dataset
+mkdir -p submodules/vista/image_folder
+cp -r $DATA_PATH/image_folder/* submodules/vista/image_folder/
+
+export PYTHONPATH=$PWD
+
+python -m dreamdrive.diffusion.sample \
+--n_frames 40 \
+--height 576 \
+--width 1024 \
+--n_steps 50 \
+--fps 12
